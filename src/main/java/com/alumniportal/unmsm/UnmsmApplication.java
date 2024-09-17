349ada2ab58895be9147106a896004ed94a14da6
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2024-09-17 13:09:08 -0500

    first commit

---
+++ src/main/java/com/alumniportal/unmsm/UnmsmApplication.java
@@ -0,0 +1,13 @@
+package com.alumniportal.unmsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnmsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnmsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alumniportal/unmsm/UnmsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alumniportal.unmsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnmsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
