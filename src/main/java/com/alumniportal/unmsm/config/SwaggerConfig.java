9222aa5ba14d5b2141f0c18c4ce1f66f49455a8b
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2025-12-05 14:39:46 -0500

    Fix Swagger Mixed Content issue using relative server url

--- src/main/java/com/alumniportal/unmsm/config/SwaggerConfig.java
+++ src/main/java/com/alumniportal/unmsm/config/SwaggerConfig.java
@@ -3,17 +3,19 @@ package com.alumniportal.unmsm.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("/")))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme()
